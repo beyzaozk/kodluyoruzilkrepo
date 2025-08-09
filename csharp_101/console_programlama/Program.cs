bca7c9b96838a98cd05dc7718e20a501ea078787
beyzaozk <byz51ozk@gmail.com>
2025-08-10 01:01:12 +0300

    csharp_101 console programlama

---
+++ csharp_101/console_programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("isminizi giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("soyisminizi giriniz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
+
