c051af1becf50e53762b1d4548585b099aed12dd
beyzaozk <byz51ozk@gmail.com>
2025-09-01 16:52:47 +0300

    döngüler

---
+++ csharp_101/donguler/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // ekrandan girilen sayıya kadar olan tek sayıları yazdır.
+            Console.Write("Bir sayi giriniz:");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            //1 ile 1000 arasındaki tek ve çift sayıların kendi içlerinde toplamlarını ekrana yazdır. 
+            int tektoplam = 0;
+            int cifttoplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tektoplam += i;
+                }
+                else
+                {
+                    cifttoplam += i;
+                }
+            }
+            Console.WriteLine("tek toplam: " + tektoplam);
+            Console.WriteLine("çift toplam: " + cifttoplam);
+
+            //break, continue
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
